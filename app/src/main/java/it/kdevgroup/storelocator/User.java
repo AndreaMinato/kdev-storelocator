83df6852aec6ee7912ea6eef987c282fd150e7e2
Damiano Giusti <damianogiusti95@gmail.com>
2016-04-15 17:27:23 +0200

    creata classe shared preferences e User

---
+++ app/src/main/java/it/kdevgroup/storelocator/User.java
@@ -0,0 +1,92 @@
+package it.kdevgroup.storelocator;
+
+import java.util.Date;
+
+/**
+ * Created by damiano on 15/04/16.
+ */
+public class User {
+    public static final String USERNAME_KEY = "username";
+    //    public static final String PASSWORD_KEY = "password";
+    public static final String SESSION_KEY = "session";
+    public static final String SESSION_TTL_KEY = "session_ttl";
+    public static final String NAME_KEY = "name";
+    public static final String SURNAME_KEY = "surname";
+    public static final String EMAIL_KEY = "email";
+
+    private String username;
+    //    private String password;
+    private String session;
+    private long sessionTtl;
+    private String name;
+    private String surname;
+    private String email;
+
+    public User() {
+    }
+
+    public User(String username, String session, long sessionTtl, String name, String surname, String email) {
+        this.username = username;
+        this.session = session;
+        this.sessionTtl = sessionTtl;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSession() {
+        return session;
+    }
+
+    public void setSession(String session) {
+        this.session = session;
+    }
+
+    public long getSessionTtl() {
+        return sessionTtl;
+    }
+
+    public void setSessionTtl(long sessionTtl) {
+        this.sessionTtl = sessionTtl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Determina se la sessione dell'utente è scaduta
+     * @return <b>true</b> se è scaduta<br><b>false</b> se è ancora attiva
+     */
+    public boolean isSessionExpired() {
+        return new Date(sessionTtl).after(new Date());
+    }
+}
