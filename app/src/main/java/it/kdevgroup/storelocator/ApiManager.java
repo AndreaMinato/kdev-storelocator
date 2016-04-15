7a7e4ce89b36fb4881a153c0dce9adb59da83394
Damiano Giusti <damianogiusti95@gmail.com>
2016-04-15 21:20:23 +0200

    commit veloce prima di andare via: implementato salvataggio utente, da fare il caricamento

--- app/src/main/java/it/kdevgroup/storelocator/ApiManager.java
+++ app/src/main/java/it/kdevgroup/storelocator/ApiManager.java
@@ -1,9 +1,16 @@
 package it.kdevgroup.storelocator;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
 /**
  * Created by damiano on 15/04/16.
  */
 public class ApiManager {
+
+    private static final String LINK_LOGIN = "http://its-bitrace.herokuapp.com/api/public/v2/login";
+
     private static ApiManager ourInstance;
 
     public static ApiManager getInstance() {
@@ -17,5 +24,27 @@ public class ApiManager {
 
     }
 
-    public
+    /**
+     * Effettua il login sul server
+     *
+     * @param username
+     * @param password non hashata, me ne occupo io
+     * @param handler  hangler
+     */
+    public void login(String username, String password, AsyncHttpResponseHandler handler) {
+        final String USERNAME = "username";
+        final String PASSWORD = "password";
+
+        RequestParams params = new RequestParams();
+        params.add(USERNAME, username);
+        params.add(PASSWORD, toBase64Sha512(password));
+
+        AsyncHttpClient httpClient = new AsyncHttpClient();
+        httpClient.post(LINK_LOGIN, params, handler);
+    }
+
+    private String toBase64Sha512(String password) {
+        // TODO
+        return password;
+    }
 }
