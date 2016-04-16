4a73dac619c4209265ed1b157f3a70f71072017d
michelebravo <tsam005@tecnicosuperiorekennedy.it>
2016-04-16 17:43:20 +0200

    implementato sha-512, ma restituisce un valore diverso(leggi il commento)
    //password:tsac
    // output del codice:z4PhNX7vuL3xVChQ1m2AB9Yg5AULVxXcg/SpIdNs6c5H0NE8XYXysP+DGNKHfuwvY7kxvUdBeoGlODJ6+SfaPg==
    //output di approsto:AkL6KhBcibHLVGZbs/JyBJqMCGB6nDLK/0ovxGZHojt6EepTxpdfygqKsIWz3Q4FS4wyHY4cIrP1W8nHAd8F4A==

--- app/src/main/java/it/kdevgroup/storelocator/ApiManager.java
+++ app/src/main/java/it/kdevgroup/storelocator/ApiManager.java
@@ -1,8 +1,14 @@
 package it.kdevgroup.storelocator;
 
+import android.util.Base64;
+import android.util.Log;
+
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by damiano on 15/04/16.
@@ -10,6 +16,7 @@ import com.loopj.android.http.RequestParams;
 public class ApiManager {
 
     private static final String LINK_LOGIN = "http://its-bitrace.herokuapp.com/api/public/v2/login";
+    private static final String TAG="tag";
 
     private static ApiManager ourInstance;
 
@@ -28,7 +35,7 @@ public class ApiManager {
      * Effettua il btnLogin sul server
      *
      * @param username
-     * @param password non hashata, me ne occupo io
+     * @param password me ne occupo io
      * @param handler  hangler
      */
     public void login(String username, String password, AsyncHttpResponseHandler handler) {
@@ -44,7 +51,22 @@ public class ApiManager {
     }
 
     private String toBase64Sha512(String password) {
-        // TODO
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA512");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        if (md != null) {
+            md.update(password.getBytes());
+            byte byteData[] = md.digest();
+            password = Base64.encodeToString(byteData, Base64.NO_WRAP);
+        }
+        Log.d(TAG,""+password);
         return password;
+        // TODO rivedere il codice perché ritorna un valore diverso da quello di approsto.com :(
+        //password:tsac
+        // output del codice:z4PhNX7vuL3xVChQ1m2AB9Yg5AULVxXcg/SpIdNs6c5H0NE8XYXysP+DGNKHfuwvY7kxvUdBeoGlODJ6+SfaPg==
+        //output di approsto:AkL6KhBcibHLVGZbs/JyBJqMCGB6nDLK/0ovxGZHojt6EepTxpdfygqKsIWz3Q4FS4wyHY4cIrP1W8nHAd8F4A==
     }
 }
