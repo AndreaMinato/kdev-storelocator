51b74d50eb66d35152395309ecf36fbde187f8a2
Mattia <m.contin.dev@gmail.com>
2016-05-04 16:58:28 +0200

    mini-fix

--- app/src/main/java/it/kdevgroup/storelocator/PagerManager.java
+++ app/src/main/java/it/kdevgroup/storelocator/PagerManager.java
@@ -3,6 +3,7 @@ package it.kdevgroup.storelocator;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
